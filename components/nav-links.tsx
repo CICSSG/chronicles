6216b6ad0fc5cc333f5496cda55fc0f61fd1a05a
keikchoco <jeremiahnueno2017@gmail.com>
2025-03-20 19:56:33 +0800

    modified:   components/nav-links.tsx 	modified:   package-lock.json 	modified:   package.json

--- components/nav-links.tsx
+++ components/nav-links.tsx
@@ -12,23 +12,40 @@ export default function NavLinks() {
         <Link href="/">Home</Link>
       </Button>
       <Button asChild size="sm" variant={pathname == "/faculty" ? "secondary" : "ghost"}>
-        <Link href="/faculty">Faculty</Link>
+        <Link href="/faculty">The CICS</Link>
       </Button>
       <Button asChild size="sm" variant={pathname == "/officers" ? "secondary" : "ghost"}>
-        <Link href="/officers">Officers</Link>
+        <Link href="/officers">Student Government</Link>
       </Button>
       <Button asChild size="sm" variant={pathname == "/announcements" ? "secondary" : "ghost"}>
         <Link href="/announcements">Announcements</Link>
       </Button>
-      <Button asChild size="sm" variant={pathname == "/events" ? "secondary" : "ghost"}>
-        <Link href="/events">Events</Link>
-      </Button>
       <Button asChild size="sm" variant={pathname == "/documents" ? "secondary" : "ghost"}>
         <Link href="/documents">Documents</Link>
       </Button>
-      <Button asChild size="sm" variant={pathname == "/about" ? "secondary" : "ghost"}>
-        <Link href="/about">About</Link>
+      <Button asChild size="sm" variant={pathname == "/events" ? "secondary" : "ghost"}>
+        <Link href="/events">Events</Link>
       </Button>
+      {/* <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <details>
+                <summary>Documents</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><a>Executive Orders</a></li>
+                  <li><a>Resolutions</a></li>
+                  <li><a>Transparecy Reports</a></li>
+                  <li><a>Ordinances</a></li>
+                  <li><a>Formal Documents</a></li>
+                  <li><a>Expenses</a></li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div> */}
+      {/* <Button asChild size="sm" variant={pathname == "/about" ? "secondary" : "ghost"}>
+        <Link href="/about">About</Link>
+      </Button> */}
       {/* <Button asChild size="sm" variant={"outline"}>
         <Link href="/sign-in">Sign in</Link>
       </Button> */}
