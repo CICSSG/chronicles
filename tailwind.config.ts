ca3033a23982cf09de30a4d2d09f2ae52488c7ce
JEREMIAH NUENO <NJH0090@dlsud.edu.ph>
2025-05-15 22:47:47 +0800

    Landing Page Update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,9 @@ const config = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      spaceGrotesk: ["space-grotesk-bold", "sans-serif"],
+    },
     container: {
       center: true,
       padding: "2rem",
