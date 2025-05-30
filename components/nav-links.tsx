107476ce6886f4fa5042e72a263868b2bbbf4835
JEREMIAH NUENO <NJH0090@dlsud.edu.ph>
2025-05-31 02:17:50 +0800

    Contact us + nav updates

--- components/nav-links.tsx
+++ components/nav-links.tsx
@@ -18,10 +18,16 @@ import {
   DocumentArrowUpIcon,
   DocumentCheckIcon,
   DocumentTextIcon,
-  DocumentChartBarIcon
+  DocumentChartBarIcon,
+  InformationCircleIcon,
+  AcademicCapIcon,
+  BriefcaseIcon,
+  BuildingOfficeIcon,
+  ComputerDesktopIcon,
+  PresentationChartLineIcon,
+  ListBulletIcon
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
-import Image from 'next/image'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
 
 const documents = [
@@ -32,12 +38,26 @@ const documents = [
   { name: 'Resolutions', description: 'Resolutions made by CICSSG', href: '/documents/resolutions', icon: DocumentChartBarIcon },
 ]
 
+const cics = [
+  { name: 'About', description: 'More information about CICS', href: '/cics', icon: InformationCircleIcon },
+  { name: 'Programs', description: 'View available programs', href: '/cics/programs', icon: ComputerDesktopIcon },
+  { name: 'Departments', description: 'Current departments of CICS', href: '/cics/departments', icon: AcademicCapIcon },
+  { name: 'Student Organization', description: 'idk', href: '/cics/student-organization', icon: BriefcaseIcon },
+  { name: 'Admin & Staff', description: 'ewan na din', href: '/cics/admin-staff', icon: BuildingOfficeIcon },
+  { name: 'Faculty', description: 'Current faculty members', href: '/cics/faculty', icon: PresentationChartLineIcon },
+]
+
+const cicssg = [
+  { name: 'About', description: 'More information about CICSSG', href: '/cicssg', icon: InformationCircleIcon },
+  { name: 'Slate', description: 'Current officers of CICSSG', href: '/cics/slate', icon: ListBulletIcon },
+]
+
 export default function NavLinks() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <header className="w-full">
-      <nav aria-label="Global" className="mx-auto flex max-w-8xl items-center justify-between lg:p-6 lg:px-8">
+      <nav aria-label="Global" className="mx-auto flex max-w-8xl items-center justify-between lg:p-6 lg:px-4">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="">[Chronicles]</span>
@@ -54,12 +74,66 @@ export default function NavLinks() {
           </button>
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-6 ">
-          <Link href={"/cics"} className="text-sm/6 font-semibold lg:text-md/6 xl:text-xl/6">
-            The CICS
-          </Link>
-          <Link href={"/cicssg"} className="text-sm/6 font-semibold lg:text-md/6 xl:text-xl/6">
-            The CICSSG
-          </Link>
+          <Popover className="relative">
+            <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold lg:text-md/6 xl:text-xl/6">
+              The CICS
+              <ChevronDownIcon aria-hidden="true" className="size-5 flex-none text-gray-400" />
+            </PopoverButton>
+            <PopoverPanel
+              transition
+              className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+            >
+              <div className="p-4">
+                {cics.map((item) => (
+                  <div
+                    key={item.name}
+                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50"
+                  >
+                    <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                      <item.icon aria-hidden="true" className="size-6 text-gray-600 group-hover:text-blue-600" />
+                    </div>
+                    <div className="flex-auto">
+                      <a href={item.href} className="block font-semibold text-gray-900">
+                        {item.name}
+                        <span className="absolute inset-0" />
+                      </a>
+                      <p className="mt-1 text-gray-600">{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </PopoverPanel>
+          </Popover>
+          <Popover className="relative">
+            <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold lg:text-md/6 xl:text-xl/6">
+              The CICSSG
+              <ChevronDownIcon aria-hidden="true" className="size-5 flex-none text-gray-400" />
+            </PopoverButton>
+            <PopoverPanel
+              transition
+              className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+            >
+              <div className="p-4">
+                {cicssg.map((item) => (
+                  <div
+                    key={item.name}
+                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50"
+                  >
+                    <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                      <item.icon aria-hidden="true" className="size-6 text-gray-600 group-hover:text-blue-600" />
+                    </div>
+                    <div className="flex-auto">
+                      <a href={item.href} className="block font-semibold text-gray-900">
+                        {item.name}
+                        <span className="absolute inset-0" />
+                      </a>
+                      <p className="mt-1 text-gray-600">{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </PopoverPanel>
+          </Popover>
           <Link href={"/announcements"} className="text-sm/6 font-semibold lg:text-md/6 xl:text-xl/6">
             Announcements
           </Link>
@@ -123,38 +197,68 @@ export default function NavLinks() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                
                 <a
-                  href="#"
+                  href="/"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  The CICS
+                  Home
                 </a>
+
+                <Disclosure as="div" className="-mx-6">
+                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 mx-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                    The CICS
+                    <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180 mx-5" />
+                  </DisclosureButton>
+                  <DisclosurePanel className="mt-2 space-y-2 bg-gray-100 inset-shadow-sm inset-shadow-black/10">
+                    {[...cics].map((item) => (
+                      <DisclosureButton
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-lg py-2 pr-10 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </DisclosureButton>
+                    ))}
+                  </DisclosurePanel>
+                </Disclosure>
+                <Disclosure as="div" className="-mx-6">
+                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 mx-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                    The CICSSG
+                    <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180 mx-5" />
+                  </DisclosureButton>
+                  <DisclosurePanel className="mt-2 space-y-2 bg-gray-100 inset-shadow-sm inset-shadow-black/10">
+                    {[...cicssg].map((item) => (
+                      <DisclosureButton
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-lg py-2 pr-10 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </DisclosureButton>
+                    ))}
+                  </DisclosurePanel>
+                </Disclosure>
                 <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  The CICSSG
-                </a>
-                <a
-                  href="#"
+                  href="/announcements"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Announcements
                 </a>
                 
-                <Disclosure as="div" className="-mx-3">
-                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                <Disclosure as="div" className="-mx-6">
+                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 mx-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
                     Documents
-                    <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
+                    <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180 mx-5" />
                   </DisclosureButton>
-                  <DisclosurePanel className="mt-2 space-y-2">
+                  <DisclosurePanel className="mt-2 space-y-2 bg-gray-100 inset-shadow-sm inset-shadow-black/10">
                     {[...documents].map((item) => (
                       <DisclosureButton
                         key={item.name}
                         as="a"
                         href={item.href}
-                        className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
+                        className="block rounded-lg py-2 pr-10 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
                       >
                         {item.name}
                       </DisclosureButton>
@@ -163,13 +267,13 @@ export default function NavLinks() {
                 </Disclosure>
 
                 <a
-                  href="#"
+                  href="/events"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Events
                 </a>
                 <a
-                  href="#"
+                  href="/contact-us"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Contact Us
