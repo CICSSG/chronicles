804dde26150e4aef7666ac36ee09f42e48415a32
keikchoco <jeremiahnueno2017@gmail.com>
2025-06-03 16:20:08 +0800

    up

---
+++ utils/get-table-rows.ts
@@ -0,0 +1,39 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+);
+
+export async function GetTableRows(){
+    const tables = [
+    'announcements',
+    'documents',
+    'events',
+    'officers',
+    'profiles'
+  ];
+  const rowCounts: { [key: string]: number | null } = {};
+  for (const table of tables){
+    const { count, error } = await supabase.from(table).select('*', {
+      count: 'exact'
+    });
+    if (error) {
+      return new Response(JSON.stringify({
+        error: error.message
+      }), {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        status: 400
+      });
+    }
+    rowCounts[table] = count;
+  }
+  return new Response(JSON.stringify(rowCounts), {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    status: 200
+  });
+}
