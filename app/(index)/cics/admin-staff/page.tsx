1886e83c62b1e4d4546193995f662ad384b292cb
JEREMIAH NUENO <NJH0090@dlsud.edu.ph>
2025-06-15 22:01:31 +0800

    user adminstaff bg correction

--- app/(index)/cics/admin-staff/page.tsx
+++ app/(index)/cics/admin-staff/page.tsx
@@ -31,84 +31,81 @@ export default function AdminStaff() {
   }, []);
 
   return (
-    <div className="flex flex-col gap-8 p-6">
-      <div className="flex flex-col gap-6">
-        <h1 className="text-3xl">Admin & Staff</h1>
-        <p className="text-lg font-normal">
-          The College of Information and Computer Studies (CICS) is a leader in
-          technological education, committed to academic excellence, innovation,
-          and ethical standards. We foster a dynamic and inclusive environment
-          where students, faculty, and industry partners work together to
-          address and solve intricate technological issues. CICS inspires and
-          prepares future leaders who excel in their professions and make
-          meaningful contributions to the global digital landscape.
-        </p>
-      </div>
-      <hr />
-
-      <div className="flex flex-row justify-around">
-        {/* Dean */}
-        <div className="flex flex-row items-center gap-4">
-          <Image
-            src={
-              documents && documents.dean.image !== ""
-                ? documents.dean.image
-                : "/images/NoImage.png"
-            }
-            alt=""
-            width={150}
-            height={150}
-            className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
-          />
-          <div className="text-center">
-            <p className="text-xl font-normal">{documents?.dean.name}</p>
-            <p className="text-lg font-semibold">Dean, CICS</p>
-          </div>
+    <div className="flex flex-col gap-8 rounded-xl bg-white/80 p-8">
+        <div className="flex flex-col gap-6">
+          <h1 className="text-3xl">Admin & Staff</h1>
+          <p className="text-lg font-normal">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ut
+            distinctio assumenda neque quia, labore optio doloremque recusandae
+            voluptates ipsam commodi delectus fugit porro, omnis iste nesciunt
+            ab. Laudantium, libero.
+          </p>
         </div>
+        <hr />
 
-        {/* Associate Dean */}
-        <div className="flex flex-row items-center gap-4">
-          <Image
-            src={
-              documents && documents.associate_dean.image !== ""
-                ? documents.associate_dean.image
-                : "/images/NoImage.png"
-            }
-            alt=""
-            width={150}
-            height={150}
-            className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
-          />
-          <div className="text-center">
-            <p className="text-xl font-normal">
-              {documents?.associate_dean.name}
-            </p>
-            <p className="text-lg font-semibold">Associate Dean, CICS</p>
-          </div>
-        </div>
-      </div>
-
-      {/* Staff */}
-      <div className="flex flex-col items-stretch w-fit m-auto my-16 gap-4">
-        {documents?.staff.map((data, i) => (
-          <div
-            key={i}
-            className={`grow flex ${i % 2 == 0 ? "flex-row" : "flex-row-reverse"} items-center gap-4`}
-          >
+        <div className="flex flex-row justify-around">
+          {/* Dean */}
+          <div className="flex flex-row items-center gap-4">
             <Image
-              src={data.image !== "" ? data.image : "/images/NoImage.png"}
+              src={
+                documents && documents.dean.image !== ""
+                  ? documents.dean.image
+                  : "/images/NoImage.png"
+              }
               alt=""
               width={150}
               height={150}
-              className="grow-0 basis-0 rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
+              className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
             />
-            <div className="grow basis-0 text-center">
-              <p className="text-xl font-normal">{data.name}</p>
-              <p className="text-lg font-semibold">{data.position}</p>
+            <div className="text-center">
+              <p className="text-xl font-normal">{documents?.dean.name}</p>
+              <p className="text-lg font-semibold">Dean, CICS</p>
             </div>
           </div>
-        ))}
+
+          {/* Associate Dean */}
+          <div className="flex flex-row items-center gap-4">
+            <Image
+              src={
+                documents && documents.associate_dean.image !== ""
+                  ? documents.associate_dean.image
+                  : "/images/NoImage.png"
+              }
+              alt=""
+              width={150}
+              height={150}
+              className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
+            />
+            <div className="text-center">
+              <p className="text-xl font-normal">
+                {documents?.associate_dean.name}
+              </p>
+              <p className="text-lg font-semibold">Associate Dean, CICS</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Staff */}
+        <div className="m-auto my-16 flex w-fit flex-col items-stretch gap-4">
+          {documents?.staff.map((data, i) => (
+            <div
+              key={i}
+              className={`flex grow ${i % 2 == 0 ? "flex-row" : "flex-row-reverse"} items-center gap-4`}
+            >
+              <Image
+                src={data.image !== "" ? data.image : "/images/NoImage.png"}
+                alt=""
+                width={150}
+                height={150}
+                className="grow-0 basis-0 rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
+              />
+              <div className="grow basis-0 text-center">
+                <p className="text-xl font-normal">{data.name}</p>
+                <p className="text-lg font-semibold">{data.position}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
   );
 }
