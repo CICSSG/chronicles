202c546440b4592d16ce7bd2d512c03512142aa4
keikchoco <jeremiahnueno2017@gmail.com>
2025-06-25 14:52:00 +0800

    [PUBLIC] Skeleton update

--- app/(index)/cics/admin-staff/page.tsx
+++ app/(index)/cics/admin-staff/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { AdminStaffDocumentData } from "@/app/(admin)/admin/admin-staff/page";
 import { PublicAdminStaffData } from "@/components/public-documents-data";
+import { AdminStaffSkeleton } from "@/components/skeleton";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-
 export default function AdminStaff() {
+  const [isLoaded, setIsLoaded] = useState(false);
   const [documents, setDocuments] = useState<AdminStaffDocumentData | null>(
     null,
   );
@@ -13,85 +14,91 @@ export default function AdminStaff() {
   useEffect(() => {
     PublicAdminStaffData().then(({ documents }) => {
       setDocuments(documents ? documents[0] : null);
+      setIsLoaded(true);
     });
   }, []);
 
   return (
     <div className="flex flex-col gap-8 xl:rounded-xl xl:bg-white/80 xl:p-8">
-        <div className="flex flex-col gap-6">
-          <h1 className="text-3xl">Admin & Staff</h1>
-          {/* <p className="text-lg font-normal">
+      <div className="flex flex-col gap-6">
+        <h1 className="text-3xl">Admin & Staff</h1>
+        {/* <p className="text-lg font-normal">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ut
             distinctio assumenda neque quia, labore optio doloremque recusandae
             voluptates ipsam commodi delectus fugit porro, omnis iste nesciunt
             ab. Laudantium, libero.
           </p> */}
-        </div>
-        <hr />
+      </div>
+      <hr />
 
-        <div className="flex flex-col *:basis-[40%] lg:mx-0 lg:flex-row justify-around gap-8">
-          {/* Dean */}
-          <div className="flex items-center gap-4">
-            <Image
-              src={
-                documents && documents.dean.image !== ""
-                  ? documents.dean.image
-                  : "/images/NoImage.png"
-              }
-              alt=""
-              width={150}
-              height={150}
-              className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
-            />
-            <div className="text-center">
-              <p className="text-xl font-normal">{documents?.dean.name}</p>
-              <p className="text-lg font-semibold">Dean, CICS</p>
-            </div>
-          </div>
+      {isLoaded ? (
+          <>
+            <div className="flex flex-col justify-around gap-8 *:basis-[40%] lg:mx-0 lg:flex-row">
+              {/* Dean */}
+              <div className="flex flex-row items-center gap-4">
+                <Image
+                  src={
+                    documents && documents.dean.image !== ""
+                      ? documents.dean.image
+                      : "/images/NoImage.png"
+                  }
+                  alt=""
+                  width={150}
+                  height={150}
+                  className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
+                />
+                <div className="text-center">
+                  <p className="text-xl font-normal">{documents?.dean.name}</p>
+                  <p className="text-lg font-semibold">Dean, CICS</p>
+                </div>
+              </div>
 
-          {/* Associate Dean */}
-          <div className="flex flex-row items-center gap-4">
-            <Image
-              src={
-                documents && documents.associate_dean.image !== ""
-                  ? documents.associate_dean.image
-                  : "/images/NoImage.png"
-              }
-              alt=""
-              width={150}
-              height={150}
-              className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
-            />
-            <div className="text-center">
-              <p className="text-xl font-normal">
-                {documents?.associate_dean.name}
-              </p>
-              <p className="text-lg font-semibold">Associate Dean, CICS</p>
-            </div>
-          </div>
-        </div>
-
-        {/* Staff */}
-        <div className="m-auto my-16 flex flex-row flex-wrap xl:*:basis-[40%] justify-around items-center gap-x-4 gap-y-10 w-full">
-          {documents?.staff.map((data, i) => (
-            <div
-              key={i}
-              className={`flex items-center gap-4 w-full`}
-            >
-              <Image
-                src={data.image !== "" ? data.image : "/images/NoImage.png"}
-                alt=""
-                width={150}
-                height={150}
-                className="grow-0 basis-0 rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
-              />
-              <div className="grow basis-0">
-                <p className={`text-xl font-normal`}>{data.name}</p>
-                <p className={`text-lg font-semibold`}>{data.position}</p>
+              {/* Associate Dean */}
+              <div className="flex flex-row items-center gap-4">
+                <Image
+                  src={
+                    documents && documents.associate_dean.image !== ""
+                      ? documents.associate_dean.image
+                      : "/images/NoImage.png"
+                  }
+                  alt=""
+                  width={150}
+                  height={150}
+                  className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
+                />
+                <div className="text-center">
+                  <p className="text-xl font-normal">
+                    {documents?.associate_dean.name}
+                  </p>
+                  <p className="text-lg font-semibold">Associate Dean, CICS</p>
+                </div>
               </div>
             </div>
-          ))}
+
+            {/* Staff */}
+            <div className="m-auto my-16 flex w-full flex-row flex-wrap items-center justify-around gap-x-4 gap-y-10 xl:*:basis-[40%]">
+              {documents?.staff.map((data, i) => (
+                <div key={i} className={`flex w-full items-center gap-4`}>
+                  <Image
+                    src={data.image !== "" ? data.image : "/images/NoImage.png"}
+                    alt=""
+                    width={150}
+                    height={150}
+                    className="grow-0 basis-0 rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
+                  />
+                  <div className="grow basis-0">
+                    <p className={`text-xl font-normal`}>{data.name}</p>
+                    <p className={`text-lg font-semibold`}>{data.position}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+      ) : (
+        <div className="flex flex-col justify-around gap-8 *:basis-[40%] lg:mx-0 lg:flex-row">
+          <AdminStaffSkeleton amount={2} />
         </div>
-      </div>
+      )}
+    </div>
   );
 }
--- app/(index)/cics/faculty/page.tsx
+++ app/(index)/cics/faculty/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { PublicFacultyData } from "@/components/public-documents-data";
+import { FacultySkeleton } from "@/components/skeleton";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -12,22 +13,26 @@ interface AdminStaffDocumentData {
 }
 
 export default function Faculty() {
-  const [documentsCS, setDocumentsCS] = useState<AdminStaffDocumentData[] | null>(
-    null,
-  );
-  const [documentsIT, setDocumentsIT] = useState<AdminStaffDocumentData[] | null>(
-    null,
-  );
+  const [isLoadedCS, setIsLoadedCS] = useState(false);
+  const [isLoadedIT, setIsLoadedIT] = useState(false);
+  const [documentsCS, setDocumentsCS] = useState<
+    AdminStaffDocumentData[] | null
+  >(null);
+  const [documentsIT, setDocumentsIT] = useState<
+    AdminStaffDocumentData[] | null
+  >(null);
 
   useEffect(() => {
     PublicFacultyData("CS").then(({ documents }) => {
       setDocumentsCS(documents ?? null);
+      setIsLoadedCS(true);
     });
     PublicFacultyData("IT").then(({ documents }) => {
       setDocumentsIT(documents ?? null);
+      setIsLoadedIT(true);
     });
+    
   }, []);
-
 
   return (
     <div className="flex flex-col gap-8 xl:rounded-xl xl:bg-white/80 xl:p-8">
@@ -47,29 +52,38 @@ export default function Faculty() {
         <h1 className="text-center text-3xl font-bold">
           Computer Science Department
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 3xl:grid-cols-4 gap-x-4 gap-y-16">
-          {documentsCS?.map((data, i) => (
-            <div key={i} className="flex flex-col items-center gap-1.5 text-center">
-              <Image
-                src={data.image.length > 2 ? data.image : "/images/NoImage.png"}
-                alt=""
-                width={200}
-                height={200}
-                className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
-              />
-              <p className="text-xl font-bold mt-1.5">{data.name}</p>
-              <p className="text-lg font-normal">{data.work_type}</p>
-              <p className="text-base font-semibold">Specializations:</p>
-              <p className="text-base font-normal">
-                {data.specialization.map((specializationData, i) => (
-                  <span key={i}>
-                    {specializationData}
-                    {i < data.specialization.length - 1 ? ", " : ""}
-                  </span>
-                ))}
-              </p>
-            </div>
-          ))}
+        <div className="3xl:grid-cols-4 grid grid-cols-1 gap-x-4 gap-y-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+          {isLoadedCS ? (
+            documentsCS?.map((data, i) => (
+              <div
+                key={i}
+                className="flex flex-col items-center gap-1.5 text-center"
+              >
+                <Image
+                  src={
+                    data.image.length > 2 ? data.image : "/images/NoImage.png"
+                  }
+                  alt=""
+                  width={200}
+                  height={200}
+                  className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
+                />
+                <p className="mt-1.5 text-xl font-bold">{data.name}</p>
+                <p className="text-lg font-normal">{data.work_type}</p>
+                <p className="text-base font-semibold">Specializations:</p>
+                <p className="text-base font-normal">
+                  {data.specialization.map((specializationData, i) => (
+                    <span key={i}>
+                      {specializationData}
+                      {i < data.specialization.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
+                </p>
+              </div>
+            ))
+          ) : (
+            <FacultySkeleton amount={4} />
+          )}
         </div>
       </div>
 
@@ -81,30 +95,37 @@ export default function Faculty() {
         </h1>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 3xl:grid-cols-4 gap-x-4 gap-y-16">
-          {documentsIT?.map((data, i) => (
-            <div key={i} className="flex flex-col items-center gap-1.5 text-center">
-              <Image
-                src={data.image.length > 2 ? data.image : "/images/NoImage.png"}
-                alt=""
-                width={200}
-                height={200}
-                className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
-              />
-              <p className="text-xl font-bold">{data.name}</p>
-              <p className="text-lg font-normal">{data.work_type}</p>
-              <p className="text-base font-semibold">Specializations:</p>
-              <p className="text-base font-normal">
-                {data.specialization.map((specializationData, i) => (
-                  <span key={i}>
-                    {specializationData}
-                    {i < data.specialization.length - 1 ? ", " : ""}
-                  </span>
-                ))}
-              </p>
-            </div>
-          ))}
-        </div>
+      <div className="3xl:grid-cols-4 grid grid-cols-1 gap-x-4 gap-y-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        {isLoadedIT ? (
+            documentsIT?.map((data, i) => (
+          <div
+            key={i}
+            className="flex flex-col items-center gap-1.5 text-center"
+          >
+            <Image
+              src={data.image.length > 2 ? data.image : "/images/NoImage.png"}
+              alt=""
+              width={200}
+              height={200}
+              className="rounded-xl border-2 border-black/60 shadow-lg shadow-blue-300"
+            />
+            <p className="text-xl font-bold">{data.name}</p>
+            <p className="text-lg font-normal">{data.work_type}</p>
+            <p className="text-base font-semibold">Specializations:</p>
+            <p className="text-base font-normal">
+              {data.specialization.map((specializationData, i) => (
+                <span key={i}>
+                  {specializationData}
+                  {i < data.specialization.length - 1 ? ", " : ""}
+                </span>
+              ))}
+            </p>
+          </div>
+        ))
+          ) : (
+            <FacultySkeleton amount={4} />
+          )}
+      </div>
     </div>
   );
 }
